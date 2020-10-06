74012c9455a0405f9eaab74e2be391cf3c6a2a01
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2020-10-06 10:26:40 +0000

    👌 IMPROVE: Optimized

--- Guessing_Game/guessing.js
+++ Guessing_Game/guessing.js
@@ -1,24 +1,24 @@
 // on Initialization....
-let correctNumber = getRandomNumber();
 
 window.onload = function () {
     document.getElementById('number-submit').addEventListener("click", playGame);
     document.getElementById('restart-game').addEventListener("click", initGame);
-
 }
-
-
-function playGame() {
-    let numberGuess = document.querySelector('#number-guess').value; //Gets the input value from the input 
-    console.log(numberGuess);
-}
-
-function initGame() {}
-
+let correctNumber = getRandomNumber();
 
 
 function getRandomNumber() {
     let randomNumber = Math.random() * 100 + 1;
     let wholeNumber = Math.floor(randomNumber);
-    console.log('Random Number:', wholeNumber);
+    console.log(wholeNumber);
+    return wholeNumber
 }
+
+function playGame() {
+    let numberGuess = document.querySelector('#number-guess').value; //Gets the input value from the input 
+    if (numberGuess < correctNumber) return console.log("Too Low 😞");
+    if (numberGuess > correctNumber) return console.log("Too High 😲");
+    else return console.log("Huh");
+}
+
+function initGame() {}
