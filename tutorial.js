bfa469b016d550b90f179c366aed7b08301f10e1
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2020-10-06 09:10:18 +0000

    Setting up Guessing Game with Vanilla JS

---
+++ tutorial.js
@@ -0,0 +1,82 @@
+// const h1 = document.querySelector('#callMoi');
+
+
+// let age = prompt('What is your age?')
+
+// h1.innerHTML = "I  am " + age + "👴years old";
+
+// let fruits = 'banana'
+// let moreFruits = 'banana,apple,blackberry,pineapple';
+
+// console.log(moreFruits);
+
+// COmmon String Methods
+// console.log(fruits.slice(2,6));
+// console.log(fruits.indexOf('na'));
+// console.log(fruits.length)
+// console.log(fruits.replace('ban', '123'))
+// console.log(fruits.toUpperCase())
+// console.log(fruits.toLowerCase())
+// console.log(fruits.split('')) Split by characters
+// console.log(moreFruits.split(','))
+
+
+// // Arrays
+// let arrayFruits = ['banana','apple','blackberry','pineapple']
+// arrayFruits = new Array('banana','apple','blackberry','pineapple')
+// console.log(arrayFruits);
+
+// for(let i; i<=arrayFruits.length; i++){
+//     console.log(i);
+// }
+
+// // Common Array Methods
+// console.log(arrayFruits.toString()); //Converts array to String
+// console.log(arrayFruits.join('-'));
+// console.log(arrayFruits.pop(), arrayFruits);
+
+
+// console.log(arrayFruits.push('myJuiceWorld'), arrayFruits);
+// arrayFruits[arrayFruits.length] = "anotherOne" //Alternate way of pushing an item to an array
+// // console.log(arrayFruits);
+
+
+// // console.log(arrayFruits.shift()); //Removes first element from array
+// arrayFruits.unshift("MoiFruites"); //Adds element to 1st position on an array
+// // console.log(arrayFruits); 
+
+// let vegetables = ['Veg_tomato', 'pepper', 'cabbage', 'lettuce']
+// let groceries = arrayFruits.concat(vegetables);
+// console.log(groceries); //Adding 2 arrays together
+
+// console.log(arrayFruits.splice(2,5));
+// console.log(groceries.reverse());
+// console.log(groceries.sort()); 1st method of sorting
+// let someNumbers = [1,2,3,4,5,6,7,8,8,200,123,213,12,435,546,34,7687,9,69,435,234,234568,9,7,705,8,36]
+// console.log(someNumbers.sort(function(a,b) {return a-b})); ///Sorting in ascending order
+// console.log(someNumbers.sort(function(a,b) {return b-a})); ///Sorting in descending order
+
+// let emptyArray = [];
+// for(let n=0; n <=12; n++){
+//     emptyArray.push(n);
+// }
+// console.log(emptyArray);
+
+
+// Objects
+// let students = { 
+//     first: "Elvis Opoku 😻", 
+//     last: "Amoako 🗃", 
+//     age: 23, 
+//     height:170,
+//     studentInfo: function(){
+//         return students.first + "\n" + students.last;
+//     }
+//  };
+// console.log(students.first);
+// console.log(students["first"]);
+// console.log(students.studentInfo());
+
+
+
+
